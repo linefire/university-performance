c2d0182f93e0b6778b5b3ea13506dd50560ab71a
LineFire <sasha.lada5@gmail.com>
2020-03-30 09:27:53 +0300

    Fix request encoding

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from json import loads
 from os import environ
 
 from flask import request
@@ -6,9 +7,9 @@ from app import app
 from app.telegram import send_message
 
 
-def start(bot_token: str):
+def start(bot_token: str, data: dict):
     if bot_token == environ['TELEGRAM_TOKEN']:
-        chat_id = request.data['message']['chat']['id']
+        chat_id = data['message']['chat']['id']
         send_message(
             bot_token,
             chat_id,
@@ -21,7 +22,8 @@ def start(bot_token: str):
 
 @app.route('/webhook/<bot_token>', methods=['POST'])
 def webhook(bot_token):
-    text = request.data['text']
+    data = loads(request.json)
+    text = data['text']
     if text == '/start':
         start(bot_token)
     return ''
