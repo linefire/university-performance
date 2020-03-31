8d40dbbbd58cf5193b9298232d3b5d597ce51f1b
LineFire <sasha.lada5@gmail.com>
2020-03-31 18:03:10 +0300

    Fix imports

--- app/__init__.py
+++ app/__init__.py
@@ -16,8 +16,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-from . import routes
 from . import model
+from . import routes
 
 if __name__ == "__main__":
     manager.run()
