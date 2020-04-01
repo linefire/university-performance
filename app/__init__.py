e55ec30e5c9d27b8b9a7b92e76cb1d86c42f4452
LineFire <sasha.lada5@gmail.com>
2020-04-01 17:38:24 +0300

    Fix2 database

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ['DATABASE_URL']
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-db.create_all()
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 from . import model
 from . import routes
+db.create_all()
 
 if __name__ == "__main__":
     manager.run()
